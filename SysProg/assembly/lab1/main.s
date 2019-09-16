59bfdfa13ca252b2fcd05fee75e15d68eee60375
Ajc605 <36259418+Ajc605@users.noreply.github.com>
2019-09-16 16:52:11 +0100

    made some comments about what the program will do, however couldnt run due to a flash file issue?

--- SysProg/assembly/lab1/main.s
+++ SysProg/assembly/lab1/main.s
@@ -2,12 +2,12 @@
 	EXPORT __main
 
 __main
-	MOV	r0, #0
-	MOV r1, #0
+	MOV	r0, #0 //Set's r0 = 0
+	MOV r1, #0 //Set's r1 = 0
 loop
-	ADD	r0, #1
-	ADD	r1, r0
-	CMP	r0, #10
+	ADD	r0, #1 //Increase the value of r0 by 1
+	ADD	r1, r0 //Increase the value of r1 by r0
+	CMP	r0, #10 //Sets Z if r0 == 10 or N is r0 < 10
 	BLT loop
 
 	B .			; Loop forever
