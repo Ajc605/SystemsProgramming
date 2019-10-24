7081ffe68868ac32dfa363f43859bbbd21604c75
Ajc605 <36259418+Ajc605@users.noreply.github.com>
2019-10-24 14:53:29 +0100

    Still stuck on task 4

--- data-storage-62M/lab4/heap.c
+++ data-storage-62M/lab4/heap.c
@@ -4,21 +4,42 @@ static void heap_up(heap_t *heap) {
 	// Write me!
 	//Start eith the least element in the heap
 	//if it's the rrot element, stop
-	if((heap->length)-1 != 0){
-		int value = heap->store[(heap->length)-1];
+	if(((heap->length)-1) != 0){
+		int position = (heap->length)-1;
+		printf("position: %d\r\n",position);
+		int value = heap->store[position];
+		int parent_p;
+		int parent;
+		//Compare it with its parent
+		if(position % 2 == 0) {
+			//even
+			parent_p = (position/2);
+			parent = heap->store[parent_p];
+		} else {
+			//odd
+			parent_p = (position)/2;
+			parent = heap->store[parent_p];
+		}
+		printf("parent: %d\r\n",parent_p);
+		//if the parent is smaller, stop
+		printf("checking %d > %d\r\n",parent,value);
+		if(parent > value){
+			printf("swap\r\n");
+			//swap the element with its parent
+			heap->store[parent_p] = value;
+			heap->store[position] = parent;
+			//with the element in its new location, go back to step 2
+			heap_up(heap);
+		}
 	} else {
-		printf("First element\r\n");
+		printf("First\r\n");
 	}
 	
-	
-	//Compare it with its parent
-	//if the parent is smaller, stop
-	//swap the element with its parent
-	//with the element in its new location, go back to step 2
 }
 
 static void heap_down(heap_t *heap) {
 	// Write me!
+	
 }
 
 void heap_init(heap_t *heap, int *store) {
@@ -28,6 +49,7 @@ void heap_init(heap_t *heap, int *store) {
 
 void heap_insert(heap_t *heap, int value) {
 	// The new element is always added to the end of a heap
+	printf("adding %d\r\n", value);
 	heap->store[(heap->length)++] = value;
 	heap_up(heap);
 }
