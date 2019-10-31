deba16a1db0aa1350f096afc62d6079ccf2bc905
Aiden Cooper <ajc605@its.york.ac.uk>
2019-10-31 12:54:20 +0000

    stuck on task 3

--- interwork/lab3/asm.s
+++ interwork/lab3/asm.s
@@ -1,6 +1,21 @@
 	AREA asm,CODE
 	
 	; Your code goes here...
+process
+	STMFD sp!, {r4, r5, r6, r7, r8, lr}
+	
+	LDR r4, [r0]
+	LDR r5, [r0 , #4]
+	LDR r6, [r0 , #8]
+	LDR r7, [r0 , #16]
+	
+	STR r6, [r5, r6]
+	
+	LDR r0, [r7]
+	BLX r7
+	
+	LDMFD sp!, {r4, r5, r6, r7, r8, lr}
+		
 	
 	END
 	
--- interwork/lab3/main.c
+++ interwork/lab3/main.c
@@ -14,10 +14,35 @@ typedef struct {
 
 #pragma pack(pop)
 
+void process(structure_t * s){};
+
+void printInteger(int *integer) {
+		
+		printf("%d\r\n", *integer);
+	}
+
 int main(void) {
 	serial_init();
 
 	// Your code goes here...
+	printf("\r\nstart\r\n");
+	
+	
+	structure_t test;
+	test.x = 5;
+	test.y = 10;
+	
+	int32_t number = 15;
+	test.ptr = &number;
+	
+	test.callback(&printInteger);
+	printf("test\r\n");
+	
+	process(&test);
+	
+	printInteger(&test.x);
+	printInteger(&test.y);
+		
 	
 	while(1);
 }
