c39495824faa34fbf805be2f884aea0c1e2d292b
Ajc605 <36259418+Ajc605@users.noreply.github.com>
2020-01-14 10:35:01 +0000

    handed in

--- DocetOS/DocetOS_sleep_wait/Flash/heap.d
+++
@@ -1,7 +0,0 @@
-.\flash\heap.o: heap.c
-.\flash\heap.o: heap.h
-.\flash\heap.o: .\OS\os.h
-.\flash\heap.o: .\OS\task.h
-.\flash\heap.o: C:\KEIL_V5\ARM\ARMCC\Bin\..\include\stdint.h
-.\flash\heap.o: C:\KEIL_V5\ARM\ARMCC\Bin\..\include\stddef.h
-.\flash\heap.o: C:\KEIL_V5\ARM\ARMCC\Bin\..\include\stdio.h
